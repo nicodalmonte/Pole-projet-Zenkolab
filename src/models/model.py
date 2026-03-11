93f016535f089b6935a21df61743bfa693ea6ec3
marcos.p-sousa@student-cs.fr <dalmonte_nic@dgx-a100>
2026-03-11 16:21:04 +0100

    modified models/model

--- src/models/model.py
+++ src/models/model.py
@@ -5,7 +5,7 @@ class DINOv3large(nn.Module):
     def __init__(self):
         super().__init__()
         self.backbone= build_backbone()
-        self.head=ClassificationHead      #li registro come due registri separati
+        self.head=ClassificationHead()      #li registro come due registri separati
     
     def forward(self,x):
         return self.head(self.backbone(x))
