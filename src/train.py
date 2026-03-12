c181b2e5cb4884c0221f0d33975bf4dc7bed40bd
nicodalmonte <nicolo.dalmonte@hotmail.com>
2026-03-12 16:27:47 +0100

    dataloader

--- src/train.py
+++ src/train.py
@@ -2,7 +2,7 @@ from src.data.dataset import LAGDataset
 from src.data.transforms import get_train_transforms, get_val_transforms
 from torch.utils.data import DataLoader
 import torch
-
+from src.models.model import DINOv3large
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
 
 training_set= LAGDataset(root= "datasets/LAG/LAG/train/", transform=get_train_transforms())
@@ -12,3 +12,5 @@ test_set= LAGDataset(root= "datasets/LAG/LAG/test/", transform=get_val_transform
 train_dataloader = DataLoader(training_set, batch_size=64, shuffle=True)
 validation_dataloader= DataLoader(validation_set, batch_size=64, shuffle=False)
 test_dataloader = DataLoader(test_set, batch_size=64, shuffle=False)    
+
+model= DINOv3large().to(device)
