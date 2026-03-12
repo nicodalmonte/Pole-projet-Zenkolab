d25f2ceb1e7a667d5aab4f6baa495216498e0c43
nicodalmonte <nicolo.dalmonte@hotmail.com>
2026-03-12 16:02:51 +0100

    added specificity and sensibility to the metrics

--- src/train.py
+++ src/train.py
@@ -1,10 +1,14 @@
 from src.data.dataset import LAGDataset
 from src.data.transforms import get_train_transforms, get_val_transforms
-import torch 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+from torch.utils.data import DataLoader
+import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
 
 training_set= LAGDataset(root= "datasets/LAG/LAG/train/", transform=get_train_transforms())
 validation_set= LAGDataset(root= "datasets/LAG/LAG/validation/", transform=get_val_transforms())
 test_set= LAGDataset(root= "datasets/LAG/LAG/test/", transform=get_val_transforms())
 
+train_dataloader = DataLoader(training_set, batch_size=64, shuffle=True)
+validation_dataloader= DataLoader(validation_set, batch_size=64, shuffle=False)
+test_dataloader = DataLoader(test_set, batch_size=64, shuffle=False)    
--- src/training/metrics.py
+++ src/training/metrics.py
@@ -1,14 +1,18 @@
-from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.metrics import f1_score, roc_auc_score, recall_score
 import numpy as np
 
 def compute_metrics(labels, probs, preds):
     auc= roc_auc_score(labels, probs)
     accuracy= np.mean(np.array(preds)==np.array(labels))
     f1= f1_score(labels, preds)
+    recall= recall_score(labels, preds)
+    specificity= recall_score(labels, preds, pos_label= 0)
 
     return {
         "auc": auc,
         "accuracy": accuracy,
-        "f1": f1
+        "f1": f1,
+        "recall": recall,
+        "specificity": specificity
     }
 
